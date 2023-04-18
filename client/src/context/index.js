ac137a99638f591b25982580e50cc73a084a5cdf
mohammed kharoda <kharodawalam@gmail.com>
2023-04-18 22:33:42 +0530

    added Campgain

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -11,6 +11,32 @@ import { ethers } from "ethers";
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract();
-  //   REMAING FUNCTIONS ARE CODED FROM HERE
-};
+  const { contract } = useContract('0x362f19481888825A1d1cABA8C10e47d3F9b57351');
+  const address = useAddress()
+  const connect = useMetamask()
+  const publishCampaign = async (form) => {
+    try{
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+      form.image
+    ])
+    console.log('contract data',data)
+  }
+  catch(error){
+    console.log('contrat call failure',error)
+  }
+}
+return(
+  <StateContext.Provider value={{
+    address,
+    contract,
+    createCampaign:publishCampaign,
+  }}>
+    {children}
+  </StateContext.Provider>
+)
+}
